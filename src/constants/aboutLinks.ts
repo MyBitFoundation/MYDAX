6f43aa385b02f5f5a985d58ca94cceac08dcf9f6
cryptnotiq <43958441+cryptnotiq@users.noreply.github.com>
2020-09-14 17:02:09 +0200

    removed dapps and contacts from constants

--- src/constants/aboutLinks.ts
+++ src/constants/aboutLinks.ts
@@ -7,6 +7,5 @@ export const AboutLinks: AboutLink[] = [
   { href: 'https://mybit.io/about', text: 'Company' },
   { href: 'https://learn.mybit.io/learn/mybit-token-myb-1', text: 'Token' },
   { href: 'https://medium.com/mybit-dapp', text: 'Blog' },
-  { href: 'https://mybit.io/tools', text: 'Other dApps' },
-  { href: 'mailto:info@mybit.io', text: 'Contact' },
+
 ]
